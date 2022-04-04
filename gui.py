9c922fd60763ae4976e15456fb9ed37394942a1e
poivronjaune <poivronjaune@gmail.com>
2022-04-03 21:45:35 -0400

    Refactor: GUI Validations

--- gui.py
+++ gui.py
@@ -63,7 +63,6 @@ def historical_data_group():
         [sg.Text("Strategy configuration")],
         [sg.Text("Starting cash:", size=labels_size), sg.Input(key="-CASH-", size=inputs_size)],
         [sg.Text("Commissions:", size=labels_size), sg.Input(key="-COMM-", size=inputs_size)],
-
     ]
     return form_layout
 
@@ -82,6 +81,50 @@ def layout_01():
     ]
     return title, layout
 
+def get_sector_value_from_gui(event, values):
+    if len(values["-SECTOR-"]) > 0:
+        # Default value
+        input_sector = values["-SECTOR-"][0]
+    else:
+        input_sector = "test"
+    
+    return input_sector
+
+def get_valid_date(values, key, default="2018-01-01"):
+    input_date_str = values[key]
+    try:
+        valid = bool(datetime.strptime(input_date_str, "%Y-%m-%d"))
+        date_str = input_date_str if valid else default
+    except Exception as e:
+        date_str = default  
+    return date_str
+
+def get_valid_int(values, key, default=0):
+    input_int_str = values[key]
+    try:
+        int_value = int(input_int_str)
+    except Exception as e:
+        int_value = default
+    return int_value
+
+
+def get_forms_values_from_gui(event, values):
+    input_hd_start  = get_valid_date(values, "-HD_START-", default="2018-01-01")
+    input_hd_end    = get_valid_date(values, "-HD_END-"  , default=datetime.today().strftime('%Y-%m-%d'))
+    input_buy_start = get_valid_date(values, "-HD_START-", default="2021-01-01")
+    input_buy_end   = get_valid_date(values, "-HD_END-"  , default=datetime.today().strftime('%Y-%m-%d'))
+    input_min_data  = get_valid_int( values, "-MIN_DATA-", default=300)
+
+    form_values = {
+        "start_historical_data":input_hd_start,                        # Historical data start_date
+        "end_historical_data":input_hd_end,                            # Historical data end_date,
+        "start_apply_strategy":input_buy_start,                        # Strategy apply date (skip price data before this date)
+        "end_apply_strategy":input_buy_end,
+        "minimum_data_required":input_min_data
+    }
+    return form_values
+        
+
 def show_gui(show_gui=False):
     title, layout = layout_01()
     window = sg.Window(title, layout)
@@ -91,38 +134,28 @@ def show_gui(show_gui=False):
         while True:
             event, values = window.read()
             if event in [sg.WIN_CLOSED, "CANCEL"]:
+                strategy_config = None
                 break
             if event in ["OK"]:
-                #print(f"VALUES['-SECTOR-']: {values['-SECTOR-']}")
-                #print(f"selected item: {values['-SECTOR-'][0]}")
-                if len(values["-SECTOR-"]) > 0:
-                    # Default value
-                    input_sector = values["-SECTOR-"][0]
-                else:
-                    input_sector = "transporation"
+                strategy_config = get_forms_values_from_gui(event, values)  
+                strategy_config["sector"] = get_sector_value_from_gui(event, values)
                 break
 
-    # TODO: Replace this section with inputs from UI
-    return {
-            "start_historical_data":"2018-01-01",                          # Historical data start_date
-            "end_historical_data":datetime.today().strftime('%Y-%m-%d'),   # Historical data end_date,
-            "start_apply_strategy":"2021-01-01",                           # Strategy apply date (skip price data before this date)
-            "minimum_data_required":300,
-            "sector":input_sector
-        }
+    window.close()
+    return strategy_config
 
 
 if __name__ == "__main__":
     run_options = None
-    if "-gui" in sys.argv:
+    if "-help" in sys.argv:
+        print(f"DEBUG: Received a -help option")
+        show_help()
+    elif "-gui" in sys.argv:
         print(f"DEBUG: Received a -gui option")
         run_options = show_gui(show_gui=True)
     elif "-nogui" in sys.argv:
         print(f"DEBUG: Received a -nogui option")
         run_options = show_gui(show_gui=False)
-    elif "-help" in sys.argv:
-        print(f"DEBUG: Received a -help option")
-        show_help()
     else:
         print(f"DEBUG: No command line arguments received")
         run_options = show_gui(show_gui=False)
