b927597ba48d14322365e22b9e403ab4c6c12039
PoivronJaune <poivronjaune@gmail.com>
2022-07-28 13:11:28 -0400

    Cleanedup strategy_logger.py

--- strategy_logger.py
+++ strategy_logger.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta, date
 import backtrader as bt
 
-# VERY SPECIFIC LOGGER, Needs improvements to become generic
 class StrategyLogger():
   
   def __init__(self, logname="default", seperator=";", strat_params=None):
@@ -68,121 +67,6 @@ class StrategyLogger():
     except Exception as e:
       log_file = None
 
-  def log_order_to_csv(self, data=None, indicators=None, max_hold_dates= None, log_type=None, order=None, order_data=None):
-    # See folder's csv_header.txt for list of columns
-    if self.log_order is None:
-      return
-
-    symbol_data = None
-    if data is not None:
-      symbol_data = data
-    
-    if order is not None:
-      symbol_data = order.data
-
-    if symbol_data is not None:
-      dt = symbol_data.datetime.date(0)
-      date_str   = f"{dt.isoformat()}"
-      symbol_str = f"{symbol_data._name}"
-      open_str   = f"{symbol_data.open[0]:.2f}"
-      high_str   = f"{symbol_data.high[0]:.2f}"
-      low_str    = f"{symbol_data.low[0]:.2f}"
-      close_str  = f"{symbol_data.close[0]:.2f}"
-      volume_str = f"{symbol_data.volume[0]:.0f}"
-
-      if max_hold_dates is not None:
-        max_hold_date_str = f"{'' if max_hold_dates[symbol_data._name] == None else max_hold_dates[symbol_data._name]}"
-      else:
-        max_hold_date_str = ''
-
-      # TODO: Replace with a loop through indicators keys
-      EMA200_str     = f"{indicators[symbol_data]['ema2'][0]:.2f}"
-      EMA20_str      = f"{indicators[symbol_data]['ema1'][0]:.2f}"
-      ATR_str        = f"{indicators[symbol_data]['atr'][0]:.2f}"
-      supertrend_str = f"{indicators[symbol_data]['supertrend'][0]:.2f}"
-    else:
-      # Something's wrong bad data available
-      raise ValueError("Bad Indicator data found.")
-      #return
-
-    if log_type is not None:
-      log_type   = f"{log_type}"
-    else:
-      log_type   = ""
-
-    if order is not None:
-      order_ref_str    = f"{order.ref}"
-      order_name_str   = f"{order.getordername()}"
-      order_type_str   = f"{order.ordtypename()}"
-      order_status_str = f"{order.getstatusname()}"
-      order_trade_id_str = f"{order.tradeid}"           # Not useful keep comment to remember we tried it (almost always zero, changes on overlaping order for same asset)
-    else:
-      order_ref_str    = ""
-      order_name_str   = ""
-      order_type_str   = ""
-      order_status_str = ""
-      order_trade_id_str = f"-1"
-
-    if order_data is not None:
-      order_price_str  = f"{order_data.price:.2f}"
-      order_size_str   = f"{order_data.size:.2f} "
-      order_value_str  = f"{order_data.value:.2f}"
-      order_comm_str   = f"{order_data.comm:.2f} "
-    else:
-      order_price_str  = f""
-      order_size_str   = f""
-      order_value_str  = f""
-      order_comm_str   = f""
-
-    sep = self.seperator
-    empty_str = ""
-    # TODO: Implement log_str for indicators based in keys available in indicators data
-    log_str  = f"{date_str}{sep}{log_type}{sep}{symbol_str}{sep}{open_str}{sep}{high_str}{sep}{low_str}{sep}{close_str}{sep}{volume_str}{sep}"
-    log_str += f"{order_ref_str}{sep}{order_name_str}{sep}{order_type_str}{sep}{order_status_str}{sep}{order_price_str}{sep}{order_size_str}{sep}{order_value_str}{sep}{order_comm_str}{sep}{max_hold_date_str}{sep}"
-    log_str += f"{EMA200_str}{sep}{EMA20_str}{sep}{ATR_str}{sep}{supertrend_str}{sep}{order_trade_id_str}{sep}\n"
-    self.log_order.write(log_str)
-
-
-  def log_trade_to_csv(self, trade=None):
-    # See folder's csv_header.txt for list of columns
-    if self.log_trade is None:
-      return
-
-    if trade is None:
-      return
-
-    sep = self.seperator
-    date_trade = trade.data.datetime.date(0)
-    trade_status = trade.status
-    trade_history_len = len(trade.history)
-    trade_symbol = trade.data._name
-    buy_index = trade.baropen - 1
-    buy_open  = trade.data._dataname.Open[buy_index]
-    buy_close = trade.data._dataname.Close[buy_index]
-
-
-    if trade.isclosed:
-      trade_close_date = trade.close_datetime()
-      trade_close_bar  = trade.barclose
-      trade_bar_len    = trade.barlen
-      sell_index = trade.barclose - 1
-      sell_open  = trade.data._dataname.Open[sell_index]
-      sell_close = trade.data._dataname.Close[sell_index]      
-    else:
-      trade_close_date = ""
-      trade_close_bar  = ""
-      trade_bar_len    = ""
-      sell_open        = ""
-      sell_close       = ""
-
-    log_str  = f"{date_trade}{sep}{trade.ref}{sep}{trade_symbol}{sep}{trade_status}{sep}{trade.size:.2f}{sep}{trade.price:.2f}{sep}{trade.value:.2f}{sep}{trade.commission:.4f}{sep}"
-    log_str += f"{trade.pnl:.2f}{sep}{trade.pnlcomm:.4f}{sep}{trade.justopened}{sep}{trade.isopen}{sep}{trade.isclosed}{sep}"
-    log_str += f"{trade.baropen}{sep}{trade.open_datetime()}{sep}{trade_close_bar}{sep}{trade_close_date}{sep}{trade_bar_len}{sep}{trade.status}{sep}"
-    log_str += f"{buy_open:.2f}{sep}{buy_close:.2f}{sep}"
-    log_str += f"{sell_open}{sep}{sell_close}{sep}"
-    log_str += f"\n"
-
-    self.log_trade.write(log_str)
 
   ###########################################################
   # Helper functions for PLACED ORDERS csv logger           #
